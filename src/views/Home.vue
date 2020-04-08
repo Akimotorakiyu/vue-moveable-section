167023844c3fac562fe493e8bc517c87cca602a9
瓦姆乌·湫曗 <autumnsilverleaves@gmail.com>
2020-04-08 14:36:28 +0800

    windows

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,39 @@
+<style lang="stylus" scoped>
+.inline-block {
+  display: inline-block;
+  width: 800px;
+  height: 600px;
+  border-radius: 1em;
+  position: relative;
+  background-color: #F6F6F6;
+}
+
+.window {
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+
+  .my-header {
+    cursor: default;
+    user-select: none;
+    background-color: black;
+    color: white;
+  }
+
+  .my-main {
+    flex: 1;
+  }
+}
+</style>
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <h1>Default</h1>
     <section class="inline-block">
       <MoveableSection>
-        <div>
-          <header>hello world</header>
-          <main @mousedown.stop>111</main>
+        <div class="window">
+          <header class="my-header">hello world</header>
+          <main class="my-main" @mousedown.stop>111</main>
         </div>
       </MoveableSection>
     </section>
@@ -25,13 +52,3 @@ export default {
 };
 </script>
 
-<style lang="stylus" scoped>
-.inline-block {
-  display: inline-block;
-  width: 800px;
-  height: 600px;
-  border-radius: 1em;
-  position: relative;
-  background-color: #F6F6F6;
-}
-</style>
